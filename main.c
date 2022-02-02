07d7e39ec0d052ad5dc51012f6d537be42f25458
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-02-02 21:27:40 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    char name[]="HARIS";
+    printf("\t%c",name[0]);
+    printf("%c",name[1]);
+    printf("%c",name[2]);
+    printf("%c",name[3]);
+    printf("%c\t",name[4]);
+
+
+
+    getch();
+    return 0;
+}
